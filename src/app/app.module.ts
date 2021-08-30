d38d0abd011392e88bdbbcb4454c293be481912f
jonathannellis <jnellis@mix.wvu.edu>
2021-08-29 20:46:23 -0400

    Created two empty navigation bars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { TopSocialNavBarComponent } from './header/top-social-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    TopSocialNavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/top-social-nav-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-top-social-nav-bar',
+    templateUrl: 'top-social-nav-bar.component.html'
+})
+export class TopSocialNavBarComponent {
+
+}
