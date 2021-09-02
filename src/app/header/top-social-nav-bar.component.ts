1d9cdc120d747494c629eac43f612be3d92bf33c
jonathannellis <jnellis@mix.wvu.edu>
2021-09-01 21:38:06 -0400

    Moved CSS for headers to seperate files

--- src/app/header/top-social-nav-bar.component.ts
+++ src/app/header/top-social-nav-bar.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-top-social-nav-bar',
-    templateUrl: 'top-social-nav-bar.component.html'
+    templateUrl: 'top-social-nav-bar.component.html',
+    styleUrls: ['top-social-nav-bar.component.css']
 })
 export class TopSocialNavBarComponent {
 
