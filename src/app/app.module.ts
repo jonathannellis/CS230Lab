7bc2dbe07485febcc7e39e142ecc6f96a7243950
jonathannellis <jnellis@mix.wvu.edu>
2021-09-08 22:21:02 -0400

    Added empty product card component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
 import { TopSocialNavBarComponent } from './header/top-social-nav-bar.component';
 import { ProductCarouselComponent } from './body/product-carousel.component';
+import { ProductCardComponent } from './body/product-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
     TopSocialNavBarComponent,
-    ProductCarouselComponent
+    ProductCarouselComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule
