7c968be6214147d6e0846837249de15b4cf8d0db
jonathannellis <jnellis@mix.wvu.edu>
2021-09-08 22:02:47 -0400

    Mostly finished carousel.

--- src/app/body/product-carousel.component.ts
+++ src/app/body/product-carousel.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-product-carousel',
     templateUrl: 'product-carousel.component.html',
+    styleUrls: ['product-carousel.component.css']
 })
 export class ProductCarouselComponent {
 
