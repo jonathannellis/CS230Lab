ae4240644ae501dd5528d23532d6c898b5a389f7
jonathannellis <jnellis@mix.wvu.edu>
2021-09-08 20:13:10 -0400

    Added carousel.  Still needs formatting improvements.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
 import { TopSocialNavBarComponent } from './header/top-social-nav-bar.component';
+import { ProductCarouselComponent } from './body/product-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    TopSocialNavBarComponent
+    TopSocialNavBarComponent,
+    ProductCarouselComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/body/product-carousel.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-product-carousel',
+    templateUrl: 'product-carousel.component.html',
+})
+export class ProductCarouselComponent {
+
+}
