15a3d5fff727673327590a8846b9ed2f732c2821
jonathannellis <jnellis@mix.wvu.edu>
2021-09-11 21:20:32 -0400

    Moved top nav bar dropdown into it's own component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { TopNavBarComponent } from './header/top-nav-bar.component';
 import { TopSocialNavBarComponent } from './header/top-social-nav-bar.component';
 import { ProductCarouselComponent } from './body/product-carousel.component';
 import { ProductCardComponent } from './body/product-card.component';
+import { TopNavBarDropdownComponent } from './header/top-nav-bar-dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
+    TopNavBarDropdownComponent,
     TopSocialNavBarComponent,
     ProductCarouselComponent,
     ProductCardComponent
---
+++ src/app/header/top-nav-bar-dropdown.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-top-nav-bar-dropdown',
+    templateUrl: 'top-nav-bar-dropdown.component.html',
+    styleUrls: ['top-nav-bar-dropdown.component.css']
+})
+export class TopNavBarDropdownComponent {
+
+}
