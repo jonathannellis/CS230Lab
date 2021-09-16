93f3bd1e6f5341c81585fd67687fce0f505b9b74
jonathannellis <jnellis@mix.wvu.edu>
2021-09-15 22:54:55 -0400

    Changed directory structure to better divide component files into related groups.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
 import { TopSocialNavBarComponent } from './header/top-social-nav-bar.component';
-import { ProductCarouselComponent } from './body/product-carousel.component';
-import { ProductCardComponent } from './body/product-card.component';
+import { ProductCarouselComponent } from './carousel/product-carousel.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 import { TopNavBarDropdownComponent } from './header/top-nav-bar-dropdown.component';
-import { PrimaryLayoutComponent } from './layout/primary-layout.component';
-import { HomeLayoutComponent } from './layout/home-layout.component';
-import { ProductCardGridFullWidthComponent } from './body/product-card-grid-full-width.component';
+import { LayoutMainComponent } from './layout/layout-main.component';
+import { LandingPageComponent } from './body/landing-page.component';
+import { ProductCardGridFullWidthComponent } from './product-card-grid/product-card-grid-full-width.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimaryLayoutComponent,
-    HomeLayoutComponent,
+    LayoutMainComponent,
+    LandingPageComponent,
     TopNavBarComponent,
     TopNavBarDropdownComponent,
     TopSocialNavBarComponent,
--- src/app/layout/primary-layout.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-primary-layout',
-    templateUrl: 'primary-layout.component.html',
-})
-export class PrimaryLayoutComponent {
-
-}
