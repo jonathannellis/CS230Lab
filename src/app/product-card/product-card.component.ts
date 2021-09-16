9f84307e9f230ac0bab1ef27ac77555a53e2b43a
jonathannellis <jnellis@mix.wvu.edu>
2021-09-16 01:20:59 -0400

    Added attributes for product name, price, and img location to product card component.

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -6,5 +6,7 @@ import { Component } from "@angular/core";
     styleUrls: ['product-card.component.css']
 })
 export class ProductCardComponent {
-
+    productName:string = "MW PBT 02 & 015 Deskmats";
+    productPrice:number = 23;
+    productImage:string = "https://ucarecdn.com/8b3b457f-3b2c-46eb-a29e-fa07b308b7be~4/nth/0/-/scale_crop/250x250/-/format/auto/-/quality/smart/-/resize/626x/";
 }
