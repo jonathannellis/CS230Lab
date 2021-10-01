7deffe9e4e1e24220045cab815fddf106730608e
jonathannellis <jnellis@mix.wvu.edu>
2021-09-30 22:57:38 -0400

    Ratings are now included in Product class and a graphical representation of this rating has been added to product card.

--- src/app/product-card-grid/product.model.ts
+++ src/app/product-card-grid/product.model.ts
@@ -6,8 +6,9 @@ export class Product {
     status:string;
     statusColor:string;
     statusVisibility:string;
+    stars:number[] = [0,0,0,0,0];
 
-    constructor({name, price, image, status} : {name:string, price:number, image:string, status:string}) {
+    constructor({name, price, image, status, rating} : {name:string, price:number, image:string, status:string, rating:number}) {
         this.name = name;
         this.price = price;
         this.image = image;
@@ -24,6 +25,21 @@ export class Product {
         else {
             this.statusColor = "bg-warning";
             this.statusVisibility = "visible";
+        }
+
+        // Create an array containing numeric value of each star in product rating (1 = full star, 0.5 = half star, 0 = zero star)
+        for (var i = 0; i < 5; i++) {
+            if (rating >= 1) {
+                this.stars[i] = 1;
+                rating--;
+            }
+            else if (rating >= 0.5) {
+                this.stars[i] = 0.5;
+                rating = rating - 0.5;
+            }
+            else {
+                this.stars[i] = 0;
+            }
         }
     }
 }
